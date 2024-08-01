095eb1cd62e9372511770ba0ec05fbc32f61e92e
Abinadi Sanchez <abinadi111@gmail.com>
2024-08-01 14:07:03 -0600

    BLoC y Flutter BLoC

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -12,11 +12,19 @@ class HomeScreen extends StatelessWidget {
       body: ListView(
 
         children: [
+
           ListTile(
             title: const Text('Cubist'),
             subtitle: const Text('Gestor de estado simple'),
             trailing: const Icon( Icons.arrow_forward_ios_rounded ),
             onTap: () => context.push('/cubits'),
+          ),
+
+          ListTile(
+            title: const Text('BLoC'),
+            subtitle: const Text('Gestor de estado compuesto'),
+            trailing: const Icon( Icons.arrow_forward_ios_rounded ),
+            onTap: () => context.push('/counter-bloc'),
           )
         ],
       ),
